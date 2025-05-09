31cdc8881c3b625f478af3691643333921620cb9
arwagadd <arwa_wail@hotmail.com>
2025-05-09 18:54:21 +0300

    removed unnecessary comments in Task

--- src/main/java/com/todo/taskservice/model/Task.java
+++ src/main/java/com/todo/taskservice/model/Task.java
@@ -25,14 +25,14 @@ public class Task {
     private Long id;
     private String title;
     private String description;
-    private TaskStatus taskStatus; //pending
-    private TaskPriority taskPriority; //medium
-    private LocalDateTime deadline; // deadline set
-    private String tagName; //no tag name
+    private TaskStatus taskStatus;
+    private TaskPriority taskPriority;
+    private LocalDateTime deadline;
+    private String tagName;
 
-    private Long boardId; //fk to board
-    private Long assignedUserId; //fk to assigned user (might add it ot not)
-    private Long createdBy; // fk to who created this task (must add)
+    private Long boardId;
+    private Long assignedUserId;
+    private Long createdBy;
 
     @CreatedDate
     @CreationTimestamp
